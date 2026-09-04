80db8a0568376bfd67c986e4441a628d0d515915
firpen <firrpen@hotmail.com>
2026-09-04 08:56:43 +0200

    created JoinScreen.tsx, added some css and a background

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
+import JoinScreen from './pages/JoinScreen'
 
 function App() {
 
   return (
     <div>
-
+      <JoinScreen />
     </div>
   )
 }
