7e5d7a7b85894c2e552179c2416dc9ff3454c609
firpen <firrpen@hotmail.com>
2026-09-09 13:43:49 +0200

    game starts and players can join

--- src/pages/JoinScreen.tsx
+++ src/pages/JoinScreen.tsx
@@ -4,17 +4,39 @@ import type { Player } from '../types/Player';
 import JoinForm from '../components/JoinForm'
 import { useEffect, useState } from 'react';
 import type { Game } from '../types/Game';
+import { Client } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
 
 function JoinScreen() {
 
     const [players, setPlayers] = useState<Player[]>([]);
     const [gameId, setGameId] = useState<string | null>(null);
-
+    
     useEffect(() => {
-        fetch("http://localhost:8080/games", { method: "POST" })
-            .then(response => response.json())
-            .then((game: Game) => setGameId(game.id));
+        fetch("http://localhost:8080/games/current")
+        .then(response => response.json())
+        .then((game: Game) => setGameId(game.id));
     }, [])
+    
+    useEffect(() => {
+        if (!gameId) return;
+        const client = new Client({
+            webSocketFactory: () => new SockJS("http://localhost:8080/websocket"),
+            onConnect: () => {
+                client.subscribe(`/topic/games/${gameId}`, (message) => {
+                    const game: Game = JSON.parse(message.body);
+                    setPlayers(game.players);
+                })
+            }
+        });
+
+        client.activate();
+    
+        return () => {
+            client.deactivate();
+        }
+
+    }, [gameId]);
 
     function handleJoin(username: string) {
         if (!gameId) return;
