c97bfacaa8506379889f328638f65e88f44b3994
firpen <firrpen@hotmail.com>
2026-09-14 19:15:44 +0200

    small fix

--- src/pages/LeaderboardScreen.tsx
+++ src/pages/LeaderboardScreen.tsx
@@ -4,6 +4,7 @@ import { API_URL } from "../constants/api";
 import "./LeaderboardScreen.css";
 import type { Game } from "../types/Game";
 import GameGrid from "../components/GameGrid";
+import PlayerList from "../components/PlayerList";
 
 interface LeaderboardScreenProps {
     onBack: () => void;
@@ -11,7 +12,7 @@ interface LeaderboardScreenProps {
     result: Result;
 }
 
-function LeaderboardScreen({game, result, onBack }: LeaderboardScreenProps) {
+function LeaderboardScreen({ game, result, onBack }: LeaderboardScreenProps) {
     const [results, setResults] = useState<Result[]>([]);
 
     // Load the results once when the screen opens.
@@ -24,14 +25,25 @@ function LeaderboardScreen({game, result, onBack }: LeaderboardScreenProps) {
     return (
         <div className="container leaderboard">
             <h1>Leader Board</h1>
+            <div className="paintingHeader">
+                <label>Target Painting</label>
+                <label>Drawing</label>
+            </div>
 
             <ol className="resultList">
                 {results.map((result) => (
                     <li key={result.id}>
-                        <GameGrid grid={game.targetPainting.grid}></GameGrid>
-                        <GameGrid grid={result.picture}></GameGrid>
-                        <span className="score">{Math.round(result.score)}%</span>
-                        <span className="time">{result.time} s</span>
+                        <div className="scoreboardPictures">
+                            <GameGrid grid={game.targetPainting.grid}></GameGrid>
+                            <GameGrid grid={result.picture}></GameGrid>
+                        </div>
+                        <div className="scoreContainer">
+                            <span className="score">{Math.round(result.score)}%</span>
+                            <span className="time">{result.time} s</span>
+                        </div>
+                        <div className="leaderboardPlayers">
+                            <PlayerList players={result.players} />
+                        </div>
                     </li>
                 ))}
             </ol>
